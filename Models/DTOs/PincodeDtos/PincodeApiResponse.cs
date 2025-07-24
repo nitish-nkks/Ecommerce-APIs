c7314e9361258b5f87c37d7bbcca2271077b5f85
Nitish Kumar Singh <nitish.singh@ibgroup.co.in>
2025-07-24 15:24:19 +0530

    Added the Pincode Validator

---
+++ Models/DTOs/PincodeDtos/PincodeApiResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace Ecommerce_APIs.Models.DTOs.PincodeDtos
+{
+    public class PincodeApiResponse
+    {
+        public string Pincode { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public bool Deliverable { get; set; }
+        public List<DeliveryOffice> DeliveryOffices { get; set; } = new();
+    }
+
+
+public class DeliveryOffice
+{
+    public string Name { get; set; } = null!;
+    public string BranchType { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string State { get; set; } = null!;
+}
+}
