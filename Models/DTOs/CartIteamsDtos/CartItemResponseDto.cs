ac2b1eaa5602a7f903d905e28aaa6b5f2cf57d88
SARANYA <msaranya182@gmail.com>
2025-08-25 21:27:07 +0530

    API updated

--- Models/DTOs/CartIteamsDtos/CartItemResponseDto.cs
+++ Models/DTOs/CartIteamsDtos/CartItemResponseDto.cs
@@ -1,11 +1,10 @@
 ﻿namespace Ecommerce_APIs.Models.DTOs.CartIteamsDtos
 {
     public class CartItemResponseDto
-    {
-        public int Id { get; set; }                  
+    {           
         public int ProductId { get; set; }           
-        public string ProductName { get; set; }    
-        public string? ProductImageUrl { get; set; }  
+        public string Name { get; set; }    
+        public string? Image { get; set; }  
         public decimal Price { get; set; }
         public decimal Discount { get; set; }
         public int Quantity { get; set; }         
